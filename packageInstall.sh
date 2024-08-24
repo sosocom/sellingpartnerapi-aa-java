70ea72d4d5e016cb9349841691f862a1f4ed0d9d
sosocom <sosocom@qq.com>
2024-08-25 00:37:52 +0800

    no message

---
+++ packageInstall.sh
@@ -0,0 +1,2 @@
+
+mvn install:install-file -Dfile=target/sellingpartnerapi-aa-java-2.0-jar-with-dependencies.jar -DgroupId=com.amazon.sellingpartnerapi -DartifactId=sellingpartnerapi-aa-java -Dversion=2.0 -Dpackaging=jar
