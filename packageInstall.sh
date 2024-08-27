6361d9c79114180cf57822adac69a650a1344f4a
sosocom <sosocom@qq.com>
2024-08-27 16:33:30 +0800

    原样照官网复制一份

--- packageInstall.sh
+++
@@ -1,2 +0,0 @@
-
-mvn install:install-file -Dfile=target/sellingpartnerapi-aa-java-2.0-jar-with-dependencies.jar -DgroupId=com.amazon.sellingpartnerapi -DartifactId=sellingpartnerapi-aa-java -Dversion=2.0 -Dpackaging=jar
